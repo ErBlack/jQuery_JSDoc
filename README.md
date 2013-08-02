c5ad5a877c6d52d119bc8568ec95f400cf85c13b
ErBlack <a.pendurin@itmozg.ru>
2013-08-02 17:19:56 +0400

    Добавлены параметры запуска

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ jQuery documentation in JSDoc
 based on:
 http://img.artlebedev.ru/tools/technogrette/soft/eclipse-spket/jquery-doc.js
 http://www.artlebedev.ru/tools/technogrette/soft/eclipse-spket/
+
+generator.php - Генерирует jsDoc из http://api.jquery.com/resources/api.xml
+
+-d - влючая deprecated методы
+-r - включая removed методы
--- generator.php
+++ generator.php
@@ -1,16 +1,23 @@
 <?php
+define('DEPRECATED',(boolean) count(getopt('d')));
+define('REMOVED',(boolean) count(getopt('r')));
+
 $xml = new SimpleXMLElement('http://api.jquery.com/resources/api.xml', null, true);
 file_put_contents('generated.js', file_get_contents('head.js') . PHP_EOL);
 
 $methods = array();
 
-foreach ($xml->entries->entry as $entry) {
+foreach($xml->entries->entry as $entry) {
 	/* @var $entry SimpleXMLElement */
 	$attr = $entry->attributes();
-	switch ($attr['type']) {
-		case 'method':
+	switch($attr['type']) {
+		case 'method' :
 			$Doc = new JSDocGenerator($entry);
-			$methods[(string)$attr['name']] = $Doc->render();
+			
+			if ($Doc->deprecated && !DEPRECATED) continue;
+			if ($Doc->removed && !REMOVED) continue;
+			
+			$methods[(string) $attr['name']] = $Doc->render();
 	}
 }
 
@@ -30,6 +37,7 @@ class JSDocGenerator {
 	 */
 	const DEPRECATED = '@deprecated';
 	/**
+	 *
 	 * @see JSDocGenerator::THORWS
 	 */
 	const EXCEPTION = '@exception';
@@ -42,6 +50,7 @@ class JSDocGenerator {
 	 */
 	const PRIV = '@private';
 	/**
+	 *
 	 * @see JSDocGenerator::RETURNS
 	 */
 	const RET = '@return';
@@ -70,37 +79,57 @@ class JSDocGenerator {
 	const DOC_END = ' */';
 	/**
 	 * xml
+	 *
 	 * @var SimpleXMLElement
 	 */
 	protected $xml = null;
 	/**
 	 * Название метода
+	 *
 	 * @var string
 	 */
 	public $name = '';
 	/**
 	 * Возвращаемое значение
+	 *
+	 * @var string
 	 */
 	public $return = '';
 	/**
+	 * Версия с которой метод deprecated
+	 *
+	 * @var string
+	 */
+	public $deprecated = false;
+	/**
+	 * Версия с которой метод удалён
+	 *
+	 * @var string
+	 */
+	public $removed = false;
+	/**
 	 * Описание метода
+	 *
 	 * @var string
 	 */
 	public $description = '';
 	/**
 	 * Версия
+	 *
 	 * @var string
 	 */
 	public $version = '';
 	/**
 	 * Аргументы
+	 *
 	 * @var array
 	 */
 	public $arguments = array();
 	/**
 	 * Генератор описания метода
-	 * @param SimpleXMLElement $xml
-	*/
+	 *
+	 * @param SimpleXMLElement $xml        	
+	 */
 	public function __construct($xml) {
 		$this->xml = $xml;
 		$this->parse();
@@ -109,22 +138,29 @@ class JSDocGenerator {
 	 * Разбирает xml на составляющие
 	 */
 	protected function parse() {
-		foreach ($this->xml->attributes() as $key => $value) {
-			if (isset($this->$key)) $this->$key = (string)$value;
+		foreach($this->xml->attributes() as $key => $value) {
+			if(property_exists($this, $key))
+				$this->$key =(string) $value;
 		}
 		
-		foreach ($this->xml as $key => $value) {
-			switch ($key) {
-				case 'desc': $this->description = (string)$value; break;
-				case 'signature': $signature = $value; break;
+		foreach($this->xml as $key => $value) {
+			switch($key) {
+				case 'desc' :
+					$this->description =(string) $value;
+					break;
+				case 'signature' :
+					$signature = $value;
+					break;
 			}
 		}
-		if (isset($signature)) {
-			foreach ($signature as $key => $value) {
+		if(isset($signature)) {
+			foreach($signature as $key => $value) {
 				
-				switch ($key) {
-					case 'added': $this->version = (string)$value; break;
-					case 'argument':
+				switch($key) {
+					case 'added' :
+						$this->version =(string) $value;
+						break;
+					case 'argument' :
 						$this->parseArgument($value);
 						break;
 				}
@@ -133,50 +169,52 @@ class JSDocGenerator {
 	}
 	/**
 	 * Разбирает аргумент
-	 * @param SimpleXMLElement $xml
+	 *
+	 * @param SimpleXMLElement $xml        	
 	 */
 	protected function parseArgument($xml) {
 		$attr = array();
-		foreach ($xml->attributes() as $key => $value) {
-			$attr[(string)$key] = (string)$value;
+		foreach($xml->attributes() as $key => $value) {
+			$attr[(string) $key] =(string) $value;
 		}
 		
-		if (!isset($attr['type'])) {
+		if(! isset($attr['type'])) {
 			$attr['type'] = array();
 			
-			foreach ($xml as $key => $value) {
+			foreach($xml as $key => $value) {
 				$type = $value->attributes();
-				if ($key == 'type') $attr['type'][] = (string)$type['name'];
+				if($key == 'type')
+					$attr['type'][] =(string) $type['name'];
 			}
 			
 			$attr['type'] = implode('|', $attr['type']);
 		}
 		
 		$attr['name'] = $this->normalizeArgument($attr['name']);
-
+		
 		$this->arguments[$attr['name']] = $this::PARAM . " {{$attr['type']}} {$attr['name']} {$xml->desc}";
 	}
 	public function render() {
 		$doc = array();
 		$doc[] = $this->description;
-
-		if ($this->version) {
+		
+		if($this->version)
 			$doc[] = $this->version();
-		}
-
+		if($this->deprecated)
+			$doc[] = $this->deprecated();
+		
 		$doc += $this->arguments;
-
+		
 		$doc[] = $this->returns();
-
+		
 		$doc = $this::DOC_BEG . PHP_EOL . $this::DOC_ITM . implode(PHP_EOL . $this::DOC_ITM, $doc) . PHP_EOL . $this::DOC_END;
-
-		return  $doc . PHP_EOL . $this->method();
-
+		
+		return $doc . PHP_EOL . $this->method();
 	}
 	protected function method() {
 		$arguments = implode(', ', array_keys($this->arguments));
 		
-		$preffix = strpos($this->name, 'jQuery') !== 0 ?  'jQuery.prototype.' : '';
+		$preffix = strpos($this->name, 'jQuery') !== 0 ? 'jQuery.prototype.' : '';
 		
 		return "{$preffix}{$this->name} = function({$arguments}){}";
 	}
@@ -186,13 +224,27 @@ class JSDocGenerator {
 	protected function version() {
 		return $this::VERSION . " {$this->version}";
 	}
+	protected function deprecated() {
+		return $this::DEPRECATED . " {$this->deprecated}";
+	}
 	/**
-	 * Приводит имя аругмента в валидное состояние
-	 * @param string $string
+	 * Приводит имя аругмента к валидной строке
+	 *
+	 * @param string $string        	
 	 */
 	protected function normalizeArgument($string) {
-		$search = array('function', 'switch', ' ', '-');
-		$replace = array('foo', 'toggle', '', '');
-		return str_replace($search, $replace, preg_replace('/\\s*\\([^()]*\\)\\s*/', '', (string)$string));
+		$search = array(
+				'function',
+				'switch',
+				' ',
+				'-' 
+		);
+		$replace = array(
+				'foo',
+				'toggle',
+				'',
+				'' 
+		);
+		return str_replace($search, $replace, preg_replace('/\\s*\\([^()]*\\)\\s*/', '',(string) $string));
 	}
 }
